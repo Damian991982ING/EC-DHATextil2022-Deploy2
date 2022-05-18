4ed1110eb15ae046976408f2c5b2064b8f05575f
Damian991982ING <ing.damian991982wwe@gmail.com>
2022-05-18 10:06:37 -0300

    commit inicial

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Ecommerce en REACT 18 y Firebase 9
         </p>
         <a
           className="App-link"
